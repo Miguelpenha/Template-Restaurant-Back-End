311c233161dd3f4ef6a191ad6ca238219acce0b0
Miguel Penha <miguel.penha1@gmail.com>
2022-08-16 04:25:27 -0300

    Teste

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node dist/index.js
+web: pm2-runtime start ecosystem.config.js --env production
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
-    name: "Sistema-Josival-Penha",
-    script: "./app.js",
+    name: "Template-Restaurant",
+    script: "./dist/app.js",
     instances: "max",
     exec_mode: "cluster_mode",
     env: {
