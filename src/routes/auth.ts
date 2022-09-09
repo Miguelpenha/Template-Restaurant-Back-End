d290e57bb393d6a40623fac1d883687605fb9358
Miguel Penha <miguel.penha1@gmail.com>
2022-09-09 16:42:04 -0300

    Update auth.ts

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,17 +1,50 @@
 import express, { Request } from 'express'
+import { sign, verify, decode } from 'jsonwebtoken'
 
 const authRouter = express.Router()
 
-authRouter.get('/login', async (req: Request<{}, {}, {
+authRouter.post('/login', async (req: Request<{}, {}, {
     login: string
     password: string
 }, {}>, res) => {
     const { login, password } = req.body
 
     if (login && login === process.env.LOGIN && password && password === process.env.PASSWORD) {
-        res.json({ authenticated: true })
+        const token = sign({}, process.env.SECRET_JWT, {
+            subject: 'true',
+            expiresIn: '20s'
+        })
+        
+        res.json({ authenticated: true, token })
     } else {
         res.json({ authenticated: false })
+    }
+})
+
+authRouter.post('/verify', async (req: Request<{}, {}, {
+    token: string
+}, {}>, res) => {
+    const { token } = req.body
+    
+    if (token) {
+        if (decode(token)) {
+            try {
+                verify(token, process.env.SECRET_JWT)
+    
+                res.json({ verified: true })
+            } catch {
+                const newToken = sign({}, process.env.SECRET_JWT, {
+                    subject: 'true',
+                    expiresIn: '20s'
+                })
+    
+                res.json({ newToken })
+            }
+        } else {
+            res.json({ tokenInvalid: true })
+        }
+    } else {
+        res.json({ tokenNotFound: true })
     }
 })
 
